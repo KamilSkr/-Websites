2a92e6a7c858da0c625ee5e670ceb4b5a5ea20a4
KamilSkr <59320706+KamilSkr@users.noreply.github.com>
2020-04-29 21:56:17 +0200

    Delete main.js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-function slideshow(){
-    var x = document.getElementById('check');
-    if(x.style.display === "none"){
-        x.style.display = " block";
-    }
-    else{
-        x.style.display =  "none";
-    }
-};
